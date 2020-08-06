81701a3c96c940b8c3bb714af141dd68bbfd2c2c
Guilherme Barasuol Scherner <36202255+GuilhermeScherner@users.noreply.github.com>
2020-08-06 16:17:16 -0300

    connect server with app web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,34 +3,54 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import { createConnection } from 'net'
+import api from '../../services/api'
 
-function TeacherItem(){
+export interface Teacher {
+  id: number;
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string,
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function createNewConnection(){
+    api.post('/connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return(
     <article className="teacher-item">
     <header>
-      <img src="https://avatars3.githubusercontent.com/u/36202255?s=460&u=769ba59c2408ca4921c9e35a28bcdab1fccf6223&v=4" alt="Guilherme"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Guilherme Scherner</strong>
-        <span>Quimica</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-      <br />
-      <br />
-       Inventore exercitationem necessitatibus reiciendis ullam quod nisi incidunt explicabo voluptatem! Rerum optio doloribus molestias, voluptatibus harum odit fugit similique delectus numquam sequi?
+    {teacher.bio}
     </p>
 
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 80,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   )
